9a687ea2d02176677e729a6c5ebccb02aeb90b0a
Jggilbert <75323365+Jggilbert@users.noreply.github.com>
2022-02-20 19:36:00 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+ function toggleFunction() {
+    var b = document.getElementById("myDIV");
+     if (b.style.visibility === "none") {
+      b.style.visibility = "block";
+    } else {
+      b.style.visibility = "none";
+    }
+  }
