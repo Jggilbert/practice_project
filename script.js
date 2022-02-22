ebb5ab5118c7e1cd91df3d6f92626084d76cbdd5
Jggilbert <75323365+Jggilbert@users.noreply.github.com>
2022-02-22 13:35:24 -0600

    second commit changed JavaScript

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
- function toggleFunction() {
-    var b = document.getElementById("myDIV");
-     if (b.style.visibility === "none") {
-      b.style.visibility = "block";
-    } else {
-      b.style.visibility = "none";
-    }
-  }
+// var button = document.getElementById("toggle").innerHTML;
+// var myDIV = document.getElementById("myDiv").innerHTML;
+// var boolean = false;
+ 
+//  function toggleFunction() {
+//    if (boolean) {
+//      myDIV.setAttribute("class", "hide")
+//      boolean = false;
+//    }
+//    else {
+//      myDIV.setAttribute("class", "show")
+//      boolean = true;
+//    }
+//  }
+
+//  button.addEventListener("click", toggleFunction, true);
